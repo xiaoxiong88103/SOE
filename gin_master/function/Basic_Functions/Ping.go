109d1f3807d12d3364e67489d08605a44095c131
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-22 17:53:19 +0800

    增加了读取基础功能的配置来更灵活的配置

--- gin_master/function/Basic_Functions/Ping.go
+++ gin_master/function/Basic_Functions/Ping.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-ping/ping"
+	"influxdb/gin_master/function/Basic_Functions/bf_ini"
 	"net/http"
 	"time"
 )
@@ -19,43 +20,6 @@ import (
 // @Failure 400 {object} string "请求参数错误"
 // @Failure 500 {object} string "内部服务器错误"
 // @Router /basic/ping [get]
-//
-//	func Ping_node(c *gin.Context) {
-//		ip := c.Query("ip")
-//		if ip == "" {
-//			c.JSON(http.StatusBadRequest, gin.H{"error": "IP地址参数缺失"})
-//			return
-//		}
-//
-//		pinger, err := ping.NewPinger(ip)
-//		if err != nil {
-//			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("创建ping请求失败: %v", err)})
-//			return
-//		}
-//
-//		pinger.Count = 3                  // 发送ping的次数
-//		pinger.Timeout = time.Second * 10 // 超时时间
-//		pinger.SetPrivileged(true)        // 在unix系统上需要root权限
-//		//用来做异步请求 防止ping的时间过长导致卡顿
-//		go func() {
-//			// 执行ping操作
-//			err = pinger.Run()
-//			if err != nil {
-//				c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("执行ping失败: %v", err)})
-//				return
-//			}
-//
-//			stats := pinger.Statistics() // 获取统计结果
-//			c.JSON(http.StatusOK, gin.H{
-//				"ip":           ip,                    // 目标 IP 地址，是 `ping` 操作的对象
-//				"rtt":          stats.AvgRtt.String(), // "rtt" 代表往返时间（Round-Trip Time），这是 `ping` 命令发送数据到接收回应所花费的平均时间。这里的值是平均往返时间的字符串表示。
-//				"packets_sent": stats.PacketsSent,     // "packets_sent" 表示发送的数据包数量。这是执行 `ping` 操作时，实际尝试发送到目标 IP 地址的 ICMP 数据包总数。
-//				"packets_recv": stats.PacketsRecv,     // "packets_recv" 表示接收到的数据包数量。这是从目标 IP 地址成功接收到的 ICMP 响应数据包总数。如果这个数值小于 "packets_sent"，可能表示网络中存在丢包。
-//			})
-//		}()
-//
-//		c.JSON(http.StatusAccepted, gin.H{"message": "Ping请求已经发送，请稍后查看结果", "code": "200"})
-//	}
 func Ping_node(c *gin.Context) {
 	ip := c.Query("ip")
 	if ip == "" {
@@ -63,34 +27,38 @@ func Ping_node(c *gin.Context) {
 		return
 	}
 
-	// 发送 Ping 操作到 goroutine
-	go func() {
-		pinger, err := ping.NewPinger(ip)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("创建ping请求失败: %v", err)})
-			return
-		}
+	pinger, err := ping.NewPinger(ip)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("创建ping请求失败: %v", err)})
+		return
+	}
+	count, err := bf_ini.Get_config_int("ping", "count")
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": err})
+		return
+	}
+	timeout, err := bf_ini.Get_config_int("ping", "timeout")
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": err})
+		return
+	}
 
-		pinger.Count = 3                  // 发送ping的次数
-		pinger.Timeout = time.Second * 10 // 超时时间
-		pinger.SetPrivileged(true)        // 在unix系统上需要root权限
+	pinger.Count = count                                  // 发送ping的次数
+	pinger.Timeout = time.Second * time.Duration(timeout) // 超时时间
+	pinger.SetPrivileged(true)                            // 在unix系统上需要root权限
+	// 执行ping操作
+	err = pinger.Run()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("执行ping失败: %v", err)})
+		return
+	}
 
-		// 执行ping操作
-		err = pinger.Run()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("执行ping失败: %v", err)})
-			return
-		}
+	stats := pinger.Statistics() // 获取统计结果
+	c.JSON(http.StatusOK, gin.H{
+		"ip":           ip,                    // 目标 IP 地址，是 `ping` 操作的对象
+		"rtt":          stats.AvgRtt.String(), // "rtt" 代表往返时间（Round-Trip Time），这是 `ping` 命令发送数据到接收回应所花费的平均时间。这里的值是平均往返时间的字符串表示。
+		"packets_sent": stats.PacketsSent,     // "packets_sent" 表示发送的数据包数量。这是执行 `ping` 操作时，实际尝试发送到目标 IP 地址的 ICMP 数据包总数。
+		"packets_recv": stats.PacketsRecv,     // "packets_recv" 表示接收到的数据包数量。这是从目标 IP 地址成功接收到的 ICMP 响应数据包总数。如果这个数值小于 "packets_sent"，可能表示网络中存在丢包。
+	})
 
-		stats := pinger.Statistics() // 获取统计结果
-		c.JSON(http.StatusOK, gin.H{
-			"ip":           ip,
-			"rtt":          stats.AvgRtt.String(),
-			"packets_sent": stats.PacketsSent,
-			"packets_recv": stats.PacketsRecv,
-		})
-	}()
-
-	// 立即响应客户端，告知 Ping 请求已经被接受
-	c.JSON(http.StatusAccepted, gin.H{"message": "Ping请求已经发送，请稍后查看结果"})
 }
---
+++ gin_master/function/Basic_Functions/bf_ini/key_value_ini.go
@@ -0,0 +1,37 @@
+package bf_ini
+
+import (
+	"fmt"
+	"github.com/go-ini/ini"
+	"strconv"
+)
+
+// GetINIValue 从INI文件中获取指定section和key的值
+func Get_config(section, key string) (string, error) {
+	cfg, err := ini.Load("./data_web/basic/basic.ini")
+	if err != nil {
+		return "", err
+	}
+
+	sec := cfg.Section(section)
+	if sec == nil {
+		return "", fmt.Errorf("Section not found: %s", section)
+	}
+
+	value := sec.Key(key).String()
+	return value, nil
+}
+
+func Get_config_int(ssection, skey string) (int, error) {
+	valuestring, err := Get_config(ssection, skey)
+	if err != nil {
+		return 0, err
+	}
+
+	value, err := strconv.Atoi(valuestring)
+	if err != nil {
+		return 0, fmt.Errorf("转换失败:", err)
+	}
+
+	return value, nil
+}
