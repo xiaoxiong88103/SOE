0c4b16e9e915ad05bf399eba5b1807ade4005ed2
“xiaoxiong88103” <xiaoxiong88103@hotmail.com>
2024-02-27 10:59:31 +0800

    更新了了client和master的gpu部分 和优化了npu部分

--- client/put_system/function/offline.go
+++ client/put_system/function/offline.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"influxdb/config"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -19,7 +18,7 @@ type UsageData struct {
 	IoWriteUsagePercent         float32   `json:"IoWriteUsagePercent"`
 	NetworkUploadUsagePercent   float32   `json:"NetworkUploadUsagePercent"`
 	NetworkDownloadUsagePercent float32   `json:"NetworkDownloadUsagePercent"`
-	GpuUsagePercent             float32   `json:"GpuUsagePercent"`
+	GpuUsagePercent             []string  `json:"GpuUsagePercent"`
 	NetworkConnections          int64     `json:"NetworkConnections"`
 	SystemLoadAvg               float32   `json:"SystemLoadAvg"`
 	DiskSizeGbShengyu           string    `json:"DiskSizeGbShengyu"`
@@ -27,55 +26,6 @@ type UsageData struct {
 	NpuUsagePercent             []float32 `json:"NpuUsagePercent"`
 	Time                        string    `json:"Time"`
 }
-
-// Off_line 将 &systemInfo 转换为 JSON 并存储在 './data/日期_data.log' 中
-//func Off_line(systemInfo *pb.SystemInfo) error {
-//	// 将 systemInfo 转换为 JSON
-//	jsonData, err := json.Marshal(systemInfo)
-//	if err != nil {
-//		return err
-//	}
-//
-//	// 生成当前日期的文件名
-//	fileDir := "./data/"
-//	fileName := fmt.Sprintf("%s%s_data.log", fileDir, time.Now().Format("2006-01-02"))
-//
-//	// 确保文件夹存在
-//	err = os.MkdirAll(fileDir, 0755)
-//	if err != nil {
-//		return err
-//	}
-//	openjson, err_json := config.DecodeJsonAsInt("config.json", "client_offonline_log_day")
-//	if err_json != nil {
-//		return err_json
-//	}
-//	// 删除超过xx天的文件
-//	err = deleteOldFiles(fileDir, openjson)
-//	if err != nil {
-//		return err
-//	}
-//
-//	// 打开文件以追加数据，如果文件不存在则创建
-//	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-//	if err != nil {
-//		return err
-//	}
-//	defer file.Close()
-//
-//	// 将 JSON 数据追加到文件
-//	_, err = file.Write(jsonData)
-//	if err != nil {
-//		return err
-//	}
-//
-//	// 可选：追加换行符以分隔每条记录
-//	_, err = file.WriteString("\n")
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
 
 func Off_line(data UsageData) error {
 	// 将 Json_data 接收到 然后写入到文件里
@@ -134,7 +84,7 @@ func Json_data() UsageData {
 	// 获取CPU、内存、IO读写的使用率
 	ioreading_use, iowrite_use, cpu_use, memory_use := getSystemStats()
 	// 调用获取GPU使用率的函数
-	gpu_use := float32(getGPUUsage())
+	gpu_use := getGPUUsage()
 
 	currentTime := time.Now()
 	timeStr := currentTime.Format(time.RFC3339)
@@ -162,7 +112,7 @@ func Json_data() UsageData {
 func deleteOldFiles(dirPath string, days int) error {
 	threshold := time.Now().AddDate(0, 0, -days) // 设置天数阈值
 
-	files, err := ioutil.ReadDir(dirPath)
+	files, err := os.ReadDir(dirPath)
 	if err != nil {
 		return err
 	}
