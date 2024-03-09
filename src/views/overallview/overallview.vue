9b949c42a469d14c09697bbc54ae45126dc1bd8b
德山 <2694065818@qq.com>
2024-03-10 01:36:59 +0800

    添加一些页面

---
+++ src/views/overallview/overallview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    总览数据
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/terminal/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    虚拟终端
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
